bc357e31f6a41c0c95e14f45e8b2a9c06f5c3e66
Kobsel <kobar13@gmx.de>
2024-01-25 13:00:14 +0100

    Add functionality to Operator Buttons
    simple calculation  can now be done
    Needs more bug fixes to improve functionality

--- code/calc.js
+++ code/calc.js
@@ -37,7 +37,7 @@ var Operator;
 var secondOperand;
 
 
-function operate(operator, numb1, numb2) {
+function simpleOperate(operator, numb1, numb2) {
 
     switch (operator) {
 
@@ -57,13 +57,22 @@ function operate(operator, numb1, numb2) {
 
 }
 
-console.log(operate('/', 4, 5));
+
 
 //Create Variables for the Display elements
 
 
 var inputfield = document.querySelector('input')
 var textfield = document.querySelector('p')
+
+//Create Variables to save operands
+
+var numb1;
+var numb2;
+var operator;
+
+var numb2Index = -1;
+var firstNumberSet = false;
 
 
 //Create functionality of Clear Button
@@ -79,28 +88,135 @@ clear.addEventListener('click', function(e){
 
 
 
-var buttons = document.querySelector('#Number-Pad')
-buttons.addEventListener('click', function(e){
+var numberButtons = document.querySelector('#Number-Pad')
+numberButtons.addEventListener('click', function(e){
 
     var id = e.target.id
     console.log(id)
 
     
 
-    if(id !== '/'){
+    if(id === '/'){
 
+        // I dunno
 
     } else if(id === 'Dot') {
 
         textfield.textContent = textfield.textContent + '.'
 
-
     } else {
+
+        if(firstNumberSet && numb2Index === -1) {
+
+            numb2Index = textfield.textContent.length
+
+        }
 
         inputfield.value = id
         textfield.textContent = textfield.textContent + id
+
+        
 
     }
     
 
 })
+
+
+
+//Create Functionality of Operator Buttons
+
+
+var operators   =   document.querySelector('#Operations')
+operators.addEventListener('click', function(e){
+
+    var id = e.target.id
+
+    if(id === 'Delete'){
+
+        deleteLastCharacter(textfield.textContent)
+
+    }  else if(id === 'Operate') {
+
+        const text = textfield.textContent
+        numb2 = parseInt(text.slice(numb2Index, text.length))
+
+        textfield.textContent = simpleOperate(operator, numb1, numb2)
+
+    }
+    else {
+
+        operator = id
+
+        if(!firstNumberSet){
+
+            numb1 = parseInt(textfield.textContent)
+            firstNumberSet = true
+
+        }
+      
+
+        if(wasLastButtonNumber()){
+            inputfield.value = id
+            textfield.textContent = textfield.textContent + ' ' + id + ' '
+        }
+
+
+
+    }
+
+})
+
+function deleteLastCharacter(string){
+
+
+    var length = string.length
+
+    if(wasLastButtonNumber()){
+
+        textfield.textContent = string.slice(0,length-1)
+
+    }  else {
+
+        textfield.textContent = string.slice(0,length-2)
+
+    }
+
+}
+
+function wasLastButtonNumber(){
+
+    const text = textfield.textContent
+    const lastChar = text.charAt(text.length-1) 
+    
+
+    if (lastChar === '7' ||
+        lastChar === '8' ||
+        lastChar === '9' ||
+        lastChar === '4' ||
+        lastChar === '5' ||
+        lastChar === '6' ||
+        lastChar === '1' ||
+        lastChar === '2' ||
+        lastChar === '3' ||
+        lastChar === '0' ||
+        lastChar === '/' ||
+        lastChar === ',')  {
+
+        return true
+
+    }
+
+    return false
+
+}
+
+
+
+
+function operate(string){
+
+
+
+
+}
